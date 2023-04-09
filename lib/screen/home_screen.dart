e19abf400991d9b02a7d5d1bc0aeefa66411b117
lsr <sirorong2@gmail.com>
2023-04-10 01:12:12 +0900

    FutureProvider

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:state_management/layout/default_layout.dart';
+import 'package:state_management/screen/future_provider_screen.dart';
 import 'package:state_management/screen/state_notifier_provider_screen.dart';
 import 'package:state_management/screen/state_provider_screen.dart';
 
@@ -31,6 +32,16 @@ class HomeScreen extends StatelessWidget {
               );
             },
             child: Text('StateNotifierProviderScreen'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => FutureProviderScreen(),
+                ),
+              );
+            },
+            child: Text('FutureProviderScreen'),
           ),
         ],
       ),
